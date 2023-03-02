dc4bbf9dab82dfad6d2bae5ba3f235ef403653e3
Lachie Sinclair <>
2023-03-02 01:15:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import discord, random
+from discord.ext import commands 
+
+TOKEN = "MTA3OTg5Njg4MTM5Nzc4MDYwMQ.GT3kqb.OLqOuYVN2no2ApOx2bCbO6nti4N5G4Xebj4Hwg"
+
+intents = discord.intents.defualt()
+intents.message_content =True
